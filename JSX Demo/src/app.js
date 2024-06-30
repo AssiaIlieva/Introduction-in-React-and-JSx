1ec8966e766bdb639718dc446e960ea19c3046f0
AssiaIlieva <ac17@abv.bg>
2024-06-30 21:51:00 +0300

    Render the content

--- JSX Demo/src/app.js
+++ JSX Demo/src/app.js
@@ -7,3 +7,6 @@ const rootReactElement = ReactDOM.createRoot(rootHtmlElement);
 
 //  Create basic react element
 const headingReactElement = React.createElement('h1', null, "Hello JSX from React");
+
+// Render content
+rootReactElement.render(headingReactElement);
