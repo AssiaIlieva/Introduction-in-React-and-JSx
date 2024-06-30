b0f8fa70a4baad0df687c9215d4d350e7cce0cf8
AssiaIlieva <ac17@abv.bg>
2024-06-30 21:50:30 +0300

    Initialize root React element

--- JSX Demo/src/app.js
+++ JSX Demo/src/app.js
@@ -1,2 +1,6 @@
 // Get root HTML Element
 const rootHtmlElement = document.getElementById('root');
+
+// Initialize root react element
+
+const rootReactElement = ReactDOM.createRoot(rootHtmlElement);
