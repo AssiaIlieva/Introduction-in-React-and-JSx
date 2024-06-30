3613547e113f3e83574951ea7787acb4d3d843a0
AssiaIlieva <ac17@abv.bg>
2024-06-30 21:50:47 +0300

    Create basic react element

--- JSX Demo/src/app.js
+++ JSX Demo/src/app.js
@@ -4,3 +4,6 @@ const rootHtmlElement = document.getElementById('root');
 // Initialize root react element
 
 const rootReactElement = ReactDOM.createRoot(rootHtmlElement);
+
+//  Create basic react element
+const headingReactElement = React.createElement('h1', null, "Hello JSX from React");
