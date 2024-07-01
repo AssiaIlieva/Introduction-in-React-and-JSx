61871fc78693d59a7ff8c5e1ff2e19105b2c7746
AssiaIlieva <ac17@abv.bg>
2024-07-01 11:25:58 +0300

    Create vite-demo

---
+++ vite-demo/src/MainHeader.jsx
@@ -0,0 +1,5 @@
+// Three options for function sintax #1
+
+export default function MainHeader(){
+    return <h2>Custom Component with JSX</h2>;
+}
---
+++ vite-demo/src/SecondHeader.jsx
@@ -0,0 +1,13 @@
+// Three options for function sintax #2
+
+const SecondHeader = () => {
+    return <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>;
+};
+
+// Three options for function sintax #3
+
+// const SecondHeader = function(){
+//     return <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>;
+// }
+
+export default SecondHeader;
