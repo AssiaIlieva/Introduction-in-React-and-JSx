cdb505639cab83448259b50b8ac21476c7453d3b
AssiaIlieva <ac17@abv.bg>
2024-07-01 09:12:53 +0300

    Create jsx demo

--- JSX Demo/src/app.js
+++ JSX Demo/src/app.js
@@ -6,7 +6,15 @@ const rootHtmlElement = document.getElementById('root');
 const rootReactElement = ReactDOM.createRoot(rootHtmlElement);
 
 //  Create basic react element
-const headingReactElement = React.createElement('h1', null, "Hello JSX from React");
+
+const headingReactSectionElement = (
+<header id="site-header" className="navigation">
+    <h1>Hello JSX from React</h1>
+    <h2>JSX is Awesome</h2>
+    <p>lorem 10</p>
+</header>
+);
 
 // Render content
-rootReactElement.render(headingReactElement);
+rootReactElement.render(headingReactSectionElement);
+// npx babel --watch src --out-dir build --presets react-app/prod
